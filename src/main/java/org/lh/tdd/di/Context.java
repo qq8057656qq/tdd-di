3e318a34c798798e4d5b199bface038be547dd92
leo <lvhuan2015@gmail.com>
2022-07-09 23:46:27 +0800

    通过provider重构了提供componentType和Implementation的map

--- src/main/java/org/lh/tdd/di/Context.java
+++ src/main/java/org/lh/tdd/di/Context.java
@@ -2,25 +2,33 @@ package org.lh.tdd.di;
 
 import jakarta.inject.Provider;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.util.Arrays.stream;
 
 public class Context {
     private Map<Class<?>, Provider<?>> providers = new HashMap<>();
 
-    public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
-        providers.put(type, (Provider<ComponentType>) () -> instance);
+    public <Type> void bind(Class<Type> type, Type instance) {
+        providers.put(type, (Provider<Type>) () -> instance);
     }
 
-    public <ComponentType> ComponentType get(Class<ComponentType> type) {
-        return (ComponentType) providers.get(type).get();
+    public <Type> Type get(Class<Type> type) {
+        return (Type) providers.get(type).get();
     }
 
-    public <ComponentType, ComponentImplementation extends ComponentType> void
-    bind(Class<ComponentType> type, Class<ComponentImplementation> implementationClass) {
-        providers.put(type, (Provider<ComponentType>) () -> {
+    public <Type, Implementation extends Type> void
+    bind(Class<Type> type, Class<Implementation> implementationClass) {
+        providers.put(type, (Provider<Type>) () -> {
             try {
-                return (ComponentType) ((Class<?>) implementationClass).getConstructor().newInstance();
+                Constructor<Implementation> constructor = implementationClass.getConstructor();
+                Object[] dependencies = stream(constructor.getParameters())
+                        .map(p -> get(p.getType()))
+                        .toArray(Object[]::new);
+                return (Type) constructor.newInstance(dependencies);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
--- src/test/java/org/lh/tdd/di/ContainerTest.java
+++ src/test/java/org/lh/tdd/di/ContainerTest.java
@@ -1,5 +1,7 @@
 package org.lh.tdd.di;
 
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -7,12 +9,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ContainerTest {
 
-    interface Component{
+    private Context context;
 
+    @BeforeEach
+    public void setup(){
+        context = new Context();
     }
 
-    static class ComponentWithDefaultConstructor implements Component{
+    interface Component{
+    }
+
+    class ComponentWithDefaultConstructor implements Component{
         public ComponentWithDefaultConstructor() {
+        }
+    }
+
+
+    interface Dependency{ }
+
+    class ComponentWithInjectConstructor implements Component{
+        private Dependency dependency;
+
+        @Inject
+        public ComponentWithInjectConstructor(Dependency dependency) {
+            this.dependency = dependency;
+        }
+
+        public Dependency getDependency() {
+            return dependency;
         }
     }
 
@@ -23,7 +47,7 @@ class ContainerTest {
 
         @Test
         void should_bind_type_to_a_specific_instance() {
-            Context context = new Context();
+            context = new Context();
             Component instance = new Component() {
             };
             context.bind(Component.class,instance);
@@ -42,8 +66,20 @@ class ContainerTest {
                 assertNotNull(instance);
                 assertTrue(instance instanceof ComponentWithDefaultConstructor);
             }
-            //todo no args constructor
             //todo with dependencies
+
+            @Test
+            void should_bind_type_to_a_class_with_inject_constructor() {
+                Dependency dependency = new Dependency() {
+                };
+                context.bind(Component.class,ComponentWithInjectConstructor.class);
+                context.bind(Dependency.class, dependency);
+                Component instance = context.get(Component.class);
+                assertNotNull(instance);
+                assertSame(dependency, ((ComponentWithInjectConstructor) instance).getDependency());
+            }
+
+
             //todo a -> b -> c
         }
 
@@ -68,3 +104,4 @@ class ContainerTest {
 
     }
 }
+

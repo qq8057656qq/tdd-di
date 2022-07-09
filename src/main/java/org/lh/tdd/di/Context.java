7ba36db3cb98f254150d76bf17e356e018392a32
leo <lvhuan2015@gmail.com>
2022-07-09 23:17:23 +0800

    通过provider重构了提供componentType和Implementation的map

--- src/main/java/org/lh/tdd/di/Context.java
+++ src/main/java/org/lh/tdd/di/Context.java
@@ -1,31 +1,29 @@
 package org.lh.tdd.di;
 
+import jakarta.inject.Provider;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Context {
-
-    private Map<Class<?>,Object> components = new HashMap<>();
-    private Map<Class<?>,Class<?>> componentImplementations = new HashMap<>();
+    private Map<Class<?>, Provider<?>> providers = new HashMap<>();
 
     public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
-        components.put(type,instance);
+        providers.put(type, (Provider<ComponentType>) () -> instance);
     }
 
-    public <ComponentType> ComponentType get(Class<ComponentType> typeClass) {
-        if (components.containsKey(typeClass)){
-            return (ComponentType) components.get(typeClass);
-        }
-        Class<?> implementation = componentImplementations.get(typeClass);
-        try {
-            return (ComponentType) implementation.getConstructor().newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public <ComponentType> ComponentType get(Class<ComponentType> type) {
+        return (ComponentType) providers.get(type).get();
     }
 
     public <ComponentType, ComponentImplementation extends ComponentType> void
-    bind(Class<ComponentType> typeClass, Class<ComponentImplementation> implementationClass) {
-        componentImplementations.put(typeClass,implementationClass);
+    bind(Class<ComponentType> type, Class<ComponentImplementation> implementationClass) {
+        providers.put(type, (Provider<ComponentType>) () -> {
+            try {
+                return (ComponentType) ((Class<?>) implementationClass).getConstructor().newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
